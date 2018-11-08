6fa8457a110955651d4658938e8f3d572e788e8d
elainechan <elaine.chan@outlook.com>
2018-11-07 23:49:20 -0500

    Adds edges to connect nodes.

--- asset-tracker-development/src/CircuitDiagram.js
+++ asset-tracker-development/src/CircuitDiagram.js
@@ -2,40 +2,51 @@ import React, { Component } from 'react';
 import cytoscape from 'cytoscape';
 
 class CircuitDiagram extends Component {
-  constructor(props) {
-    super(props)
-  }
 
   componentDidUpdate() {
     const {assets} = this.props;
     const elements = assets.map((v, i) => {
-	return {
-	      group: 'nodes',
-	      data: {
-		id: 'n' + v.id,
-		vender: v.vendor,
-		product: v.product
-	      },
-	      position: {
-		x: (Number(v.lng)) * 10000,
-		y: -(Number(v.lat)) * 10000
-	      },
-	      selected: false,
-	      selectable: true
-	}
-    })
+      return {
+          group: 'nodes',
+          data: {
+            id: 'n' + v.id,
+            vender: v.vendor,
+            product: v.product
+          },
+          position: {
+            x: (Number(v.lng)) * 10000,
+            y: -(Number(v.lat)) * 10000
+          },
+          selected: false,
+          selectable: true
+      };
+    });
+    const edges = assets.map((v, i) => {
+      return {
+        group: 'edges',
+        data: {
+          id: 'e' + v.id,
+          source: 'n' + v.id,
+          target: 'n' + (Number(v.id) + 1).toString()
+        },
+        style: {
+          width: 5
+        }
+      }
+    });
     window.cy = cytoscape({
-     container: document.getElementById('cy'),
-     elements: elements,
-     layout: { 
-       name: 'preset'
-     },
-     style: [{ 
-       selector: 'node', 
-       style: { 
-         'content': 'data(name)' 
+      container: document.getElementById('cy'),
+      elements: elements.concat(edges),
+      layout: { 
+      name: 'preset'
+      },
+      style: [{ 
+        selector: 'node', 
+        style: { 
+          'content': 'data(name)' 
        }
-     }]}) 
+      }]
+    }); 
   }
 
   render() {
