b864456c5c837843e84166a8cfdeaa95ee14528e
salah ahmed <salah93@crosscompute.com>
2018-11-10 15:27:55 -0500

    Remove extra libraries
    refactor code a bit to use connections data from endpoint

--- asset-tracker-development/src/CircuitDiagram.js
+++ asset-tracker-development/src/CircuitDiagram.js
@@ -1,95 +1,62 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import cytoscape from 'cytoscape';
-import popper from 'cytoscape-popper';
-import tippy from 'tippy.js';
-cytoscape.use(popper);
+
 
 class CircuitDiagram extends Component {
-
   componentDidUpdate() {
-    const {assets} = this.props;
-    const elements = assets.map((v, i) => {
-      return {
+    const {assets, connections} = this.props;
+    const nodes = assets.map((n, i) => {return {
           group: 'nodes',
           data: {
-            id: 'n' + v.id,
-            vender: v.vendor,
-            product: v.product
+            id: `${n.id}`,
+            vendor: n.vendor,
+            product: n.product
           },
           position: {
-            x: (Number(v.lng)) * 10000,
-            y: -(Number(v.lat)) * 10000
+            x: (Number(n.lng)) * 10000,
+            y: -(Number(n.lat)) * 10000
           },
           selected: false,
           selectable: true
-      };
-    });
-    const edges = assets.map((v, i) => {
-      if (i === assets.length - 1) {
-        return {
+      }}
+    );
+    const edges = connections.map((e) => {
+      const [from, to] = e;
+      return {
           group: 'edges',
           data: {
-            id: 'e' + v.id,
-            source: 'n' + v.id,
-            target: 'n1'
+            id: `${from},${to}`,
+            source: `${from}`,
+            target: `${to}`
           },
           style: {
             width: 5
           }
         }
-      }
-      return {
-        group: 'edges',
-        data: {
-          id: 'e' + v.id,
-          source: 'n' + v.id,
-          target: 'n' + (Number(v.id) + 1).toString()
-        },
-        style: {
-          width: 5
-        }
-      }
     });
+
     const cy = window.cy = cytoscape({
       container: document.getElementById('cy'),
-      elements: elements.concat(edges),
-      layout: { 
-      name: 'preset'
+      elements: {
+        nodes: nodes,
+        edges: edges,
       },
-      style: [{ 
-        selector: 'node', 
-        style: { 
-          'content': 'data(name)' 
-       }
-      }]
-    });
-    cy.nodes().forEach(node => {
-      node.popper({
-        content: () => {
-          let div = document.createElement('div');
-          div.innerHTML = node.data('product');
-          document.body.appendChild(div);
-          return div;
+      layout: {
+        name: 'preset'
+      },
+      style: [{
+        selector: 'node',
+        style: {
+          'content': 'data(product)'
         }
-      });
-    });
-    cy.nodes().map(node => {
-      return tippy(node.popperRef(), {
-        content: node.data('vendor'),
-        trigger: 'manual',
-        arrow: true,
-        placement: 'bottom',
-        hideOnClick: false,
-        multiple: true,
-        sticky: true
-      });
+      }]
     });
   }
 
   render() {
-    const cyStyle = { 'height': '400px', 'width': '400px'}
+    const style = {height: '400px', width: '400px'}
 	  return (
-      <div id='cy' style={cyStyle} />
+      <div id='cy' style={style} />
     )
   }
 }
