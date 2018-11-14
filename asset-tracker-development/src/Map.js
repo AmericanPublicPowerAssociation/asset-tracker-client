170fca8092f1be316fc5a3d89e95ff1205de9526
Rodrigo <rmguarachi@gmail.com>
2018-11-14 03:41:58 +0000

    Changed markers to layers
    Updated code to use layers. when selected data -> color changes.

--- asset-tracker-development/src/Map.js
+++ asset-tracker-development/src/Map.js
@@ -5,6 +5,49 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 
 class Map extends Component {
+  /*
+   * TODO: when clicked it should call the fn "updateSelected"
+   *
+   *
+   */
+   createLayer(assets, selectedAsset, layerName) {
+  const assetData = assets.map((a) => {
+  const isSelected = (selectedAsset !== null && selectedAsset.id === a.id) ? 1 : 0;
+    return {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [a.lng, a.lat]
+                },
+                "properties": {
+        "id": a.id,
+        "selected": isSelected
+                }
+    }
+  });
+
+  const layer = {
+    id: layerName,
+    type: "circle",
+    source: {
+      type: 'geojson',
+      data: {
+        "type": "FeatureCollection",
+        "features": assetData,
+      }
+    },
+    paint: {
+      "circle-color": [
+        "match",
+        ["get", "selected"],
+        1, "red",
+        "blue",
+      ]
+    },
+  };
+  return layer;
+   }
+
   addMarker(asset, index, selectedAsset) {
       const {updateSelected} = this.props;
       const popup = new mapboxgl.Popup()
@@ -23,9 +66,9 @@ class Map extends Component {
   setBounds(markers) {
     if (markers.length > 0) {
       return markers.reduce(function(bounds, marker) {
-                  return bounds.extend(marker.getLngLat());
+                  return bounds.extend(marker.geometry.coordinates);
               }, new mapboxgl.LngLatBounds(
-                markers[0].getLngLat(), markers[0].getLngLat()));
+                markers[0].geometry.coordinates, markers[0].geometry.coordinates));
     } else {
       return new mapboxgl.LngLatBounds([0, 0], [0, 0]);
     }
@@ -34,15 +77,23 @@ class Map extends Component {
   componentDidUpdate(prevProps, prevState) {
     const {markers, selectedAsset} = this.props;
 
-    this.markers.forEach((m) => m.remove())
-    this.markers = markers.map((m, i) => this.addMarker(m, i, selectedAsset));
-    this.markers.forEach((m) => m.addTo(this.map))
+    const layerName = 'assets'
+    const layer = this.map.getLayer(layerName)
+    console.log(layer);
+    if (layer) {
+  this.map.removeLayer(layerName);
+  this.map.removeSource(layerName);
+    }
+    this.markers = this.createLayer(markers, selectedAsset, layerName);
+    this.map.addLayer(this.markers);
 
     // if markers didn't change, don't change bounds
-    if ((this.markers.length !== prevProps.markers.length) || (
-      this.markers.some((m, i) => m.id !== prevProps.markers[i].id)
+    const points = this.markers.source.data.features;
+    const prevPoints = prevProps.markers;
+    if ((points.length !== prevPoints.length) || (
+      points.some((m, i) => m.properties.id !== prevPoints[i].id)
     )) {
-      const bounds = this.setBounds(this.markers);
+      const bounds = this.setBounds(points);
       this.map.fitBounds(bounds, {
                   padding: 20
               });
@@ -52,7 +103,7 @@ class Map extends Component {
 
   componentDidMount() {
     mapboxgl.accessToken = 'pk.eyJ1Ijoic2FsYWgtaGFsYXMiLCJhIjoiY2prdnY1Y25mMGN3cjN2cTVxa2tvbWRnZCJ9.c8uFh6KYWAi1SPF6ZRosAA';
-    fetch('http://localhost:5000/get-center.json')
+    fetch('http://138.197.69.144:5000/get-center.json')
       .then((res) => res.json())
       .then((data) => {
         const {lat, lng} = JSON.parse(data);
@@ -63,7 +114,7 @@ class Map extends Component {
               zoom: 8,
               minZoom: 10,
             });
-        this.markers = [];
+        this.markers = {};
       })
   }
 
