d7ed995e4744348f2d8852f786ff045d301d8774
Roy Hyunjin Han <rhh@crosscompute.com>
2019-01-06 06:39:00 -0500

    Remove serviceWorker.js

--- client-old/src/index.js
+++ client-old/src/index.js
@@ -7,7 +7,6 @@ import thunk from 'redux-thunk'
 
 import rootReducer from './reducers'
 import App from './components/App';
-//import * as serviceWorker from './serviceWorker';
 
 import './css/index.css';
 
@@ -20,8 +19,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-//serviceWorker.unregister();
