bbf21e7d47c467743e8dbf813a1420ae90b3060f
Polina <polina@crosscompute.com>
2019-01-10 21:52:07 +0000

    Added remaining buttons and menu icon

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,91 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Asset Tracker
+          </Typography>
+          <Button color="inherit">Maps</Button>
+          <Button color="inherit">Tables</Button>
+          <Button color="inherit">Reports</Button>
+          <Button color="inherit">Alerts</Button>
+          <Button color="inherit">Alex</Button>
+          <Button color="inherit">Sign Out</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
+
+/*import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+      flexGrow: 1,
+    },
+  grow: {
+      flexGrow: 1,
+    },
+  menuButton: {
+      marginLeft: -12,
+          marginRight: 20,
+            },
+            };
 
 const NavBar = () => {
   return (
-    <div>
+    <div >
       <AppBar position="static" color="default">
         <Toolbar>
           <Typography variant="h6" color="inherit">
             Asset Tracker
           </Typography>
+          <Button color="inherit">Sign Out</Button>
         </Toolbar>
       </AppBar>
     </div>
   );
 };
 export default NavBar;
-
+*/
