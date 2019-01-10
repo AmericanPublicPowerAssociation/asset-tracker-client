40affcb6bdde48d5d3a8303127448bad6f736aee
Polina <polina@crosscompute.com>
2019-01-10 21:13:21 +0000

    Added material-ui version of the App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react'
 import Form from './Form'
 import List from './List'
 import Post from './Post'
+import TheGrid from "./TheGrid"
+import NavBar from "./NavBar"
 import './App.css'
 
-/*
+
 class App extends Component {
   render() {
-    return 'x'
+    //return 'x'
+    return(
+    <div>
+      <NavBar/>
+      <TheGrid/>
+    </div>
+    );
   }
 }
-*/
-const App = () => (
+
+  /*const App = () => (
   <React.Fragment>
   <Form />
   <h2>Assets</h2>
@@ -19,6 +27,6 @@ const App = () => (
   <h2>Posts</h2>
   <Post />
   </React.Fragment>
-)
+)*/
 
 export default App
---
+++ client/src/components/RightPane.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default props => (
+  // checkbox list
+  <div>
+    Station
+    <br /> Substation
+    <br /> Transmission
+    <br /> Distribution <br />
+    Quality <br />
+    Capacitor
+    <br /> Regulator
+    <br />
+    Booster <br />
+    Switch <br />
+    Fuse
+    <br /> Breaker <br />
+    Recloser <br />
+    Interrupter <br />
+    Sectionalizer
+    <br /> Relay Transformer <br />
+    Distribution
+    <br /> Meter <br />
+    Residential <br />
+    Commercial <br />
+    Industrial <br />
+    Line <br />
+    Pole Busbar
+    <br /> Control
+    <br /> PLC Microcontroller <br />
+    Miscellaneous
+  </div>
+);
+
