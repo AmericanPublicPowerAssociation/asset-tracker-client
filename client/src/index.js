372cbed2fb13623501d39850865c3ec2e8c0ea6e
Roy Hyunjin Han <rhh@crosscompute.com>
2019-01-12 14:58:30 -0500

    Save bit

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,11 @@ import './index.css'
 import { forbiddenWordsTween } from './tweens'
 import thunk from 'redux-thunk'
 
-const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const enhanceStore = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   rootReducer,
-  storeEnhancers(applyMiddleware(forbiddenWordsTween, thunk)),
+  enhanceStore(applyMiddleware(forbiddenWordsTween, thunk)),
 )
 
 ReactDOM.render(
