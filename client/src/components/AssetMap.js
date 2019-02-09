71f30b174bf668e9c6f08dfd6a1a0bfb97eac561
Salah Ahmed <salah93@crosscompute.com>
2019-02-09 17:45:05 +0000

    add mapstyle to state

--- client/src/components/AssetMap.js
+++ client/src/components/AssetMap.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import ReactMapGL from 'react-map-gl'
 import { fromJS } from 'immutable'
-import MAP_STYLE from '../datasets/map-style-basic-v8.json'
+import MAP_STYLE from '../datasets/map-style-satellite-streets.json'
 
-import meterExample from '../datasets/toy-meter.json'
+import meterExample from '../datasets/example-meter.geojson'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
 
@@ -12,44 +12,48 @@ class AssetMap extends Component {
     viewport: {
       width: '100%',
       height: '100%',
-      longitude: -122,
-      latitude: 37,
-      zoom: 8,
-    }
+      longitude: -80,
+      latitude: 35,
+      zoom: 6,
+    },
+    mapStyle: MAP_STYLE
   }
 
-  render () {
-    //const mapStyle = lineExample;
-    const mapStyle = fromJS({
-        ...MAP_STYLE,
+  componentDidMount() {
+    const {mapStyle} = this.state;
+    console.log(mapStyle)
+    const style = fromJS({
+        ...mapStyle,
         sources: {
-          ...MAP_STYLE.sources,
+          ...mapStyle.sources,
           meters: {
               type: 'geojson',
               data: meterExample,
           },
         },
         layers: [
-            ...MAP_STYLE.layers,
+            ...mapStyle.layers,
             {
               id: 'my-meters',
               type: 'circle',
               source: 'meters',
               paint: {
                 'circle-color': 'red',
-                'circle-radius': 8
+                'circle-radius': 4
               }
             }
         ]
     });
+    this.setState({mapStyle: style})
+  }
 
+  render () {
     return (
-
       <ReactMapGL
         //mapStyle='mapbox://styles/mapbox/satellite-v9'
         // mapStyle='mapbox://styles/mapbox/streets-v9'
         //mapStyle='mapbox://styles/invisibleroads/cjrxqr50m0q1j1fp91ydmnoqv'
-        mapStyle={ mapStyle }
+        mapStyle={ this.state.mapStyle }
         {...this.state.viewport}
         onViewportChange={viewport => this.setState({viewport})}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
