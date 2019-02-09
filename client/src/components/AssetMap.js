46b830f78c67e14c3dc57628b56b79291cd347a2
Salah Ahmed <salah93@crosscompute.com>
2019-02-09 17:13:20 +0000

    add points with base map
    in order to add points you need to load the base map as seen here
    https://github.com/uber/react-map-gl/blob/master/examples/zoom-to-bounds/src/map-style.js

--- client/src/components/AssetMap.js
+++ client/src/components/AssetMap.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import ReactMapGL from 'react-map-gl'
+import { fromJS } from 'immutable'
+import MAP_STYLE from '../datasets/map-style-basic-v8.json'
 
-import lineExample from '../datasets/example-line.geojson'
+//import lineExample from '../datasets/example-line.geojson'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
 
@@ -17,11 +19,59 @@ class AssetMap extends Component {
   }
 
   render () {
-    console.log(process.env)
+    //const mapStyle = lineExample;
+    const mapStyle = fromJS({
+        ...MAP_STYLE,
+        sources: {
+          ...MAP_STYLE.sources,
+          points: {
+              type: 'geojson',
+              data: {
+                  type: 'FeatureCollection',
+                  features: [
+                      {type: 'Feature', geometry: {type: 'Point', coordinates: [-122.45, 37.78]}},
+                  ]
+              }
+          },
+          lines: {
+              type: 'geojson',
+              data: {
+                  type: 'FeatureCollection',
+                  features: [
+                      {type: 'Feature', geometry: {type: 'LineString', coordinates: [[-122.45, 37.78], [-132.45, 40.78]]}},
+                  ]
+              }
+
+          }
+        },
+        layers: [
+            ...MAP_STYLE.layers,
+            {
+                id: 'my-layer',
+                type: 'circle',
+                source: 'points',
+                paint: {
+                    'circle-color': '#f00',
+                    'circle-radius': 14
+                }
+            },
+            {
+                id: 'my-lines',
+                type: 'line',
+                source: 'lines',
+                paint: {
+                  'line-color': 'yellow',
+                  'line-width': 7
+                }
+            }
+        ]
+    });
+
     return (
       <ReactMapGL
         //mapStyle='mapbox://styles/mapbox/satellite-v9'
-        mapStyle='mapbox://styles/mapbox/streets-v9'
+        //mapStyle='mapbox://styles/mapbox/streets-v9'
+        mapStyle={ mapStyle }
         {...this.state.viewport}
         onViewportChange={viewport => this.setState({viewport})}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
