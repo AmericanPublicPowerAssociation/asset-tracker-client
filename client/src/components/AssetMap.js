5091375e0607325936ce147f97f1b6b0e132ffbd
Salah Ahmed <salah93@crosscompute.com>
2019-02-09 16:45:21 +0000

    add streets layout to map

--- client/src/components/AssetMap.js
+++ client/src/components/AssetMap.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import ReactMapGL from 'react-map-gl'
 
+import lineExample from '../datasets/example-line.geojson'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 class AssetMap extends Component {
   state = {
     viewport: {
-      // width: '100%',
-      // height: '100%',
+      width: '100%',
+      height: '100%',
       longitude: -122,
       latitude: 37,
       zoom: 8,
@@ -16,7 +20,10 @@ class AssetMap extends Component {
     console.log(process.env)
     return (
       <ReactMapGL
+        //mapStyle='mapbox://styles/mapbox/satellite-v9'
+        mapStyle='mapbox://styles/mapbox/streets-v9'
         {...this.state.viewport}
+        onViewportChange={viewport => this.setState({viewport})}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       />
     )
