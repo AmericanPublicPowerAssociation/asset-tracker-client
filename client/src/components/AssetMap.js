0cf913ac0230e8998819d0404898e57509dc68ca
Roy Hyunjin Han <rhh@crosscompute.com>
2019-02-13 11:45:32 -0500

    Set width and radius range from 1 to 10

--- client/src/components/AssetMap.js
+++ client/src/components/AssetMap.js
@@ -5,48 +5,16 @@ import {
   KEY_PREFIX,
   MAP_STYLE,
 } from '../constants'
-import LINE_GEOJSON from '../datasets/toy-line.geojson'
-import METER_GEOJSON from '../datasets/toy-meter.geojson'
-// import LINE_GEOJSON from '../datasets/line.geojson'
-// import METER_GEOJSON from '../datasets/meter.geojson'
+import LINE_GEOJSON from '../datasets/line.geojson'
+import METER_GEOJSON from '../datasets/meter.geojson'
 
 class AssetMap extends Component {
   state = {
     viewport: {
       longitude: -79.7919754,
       latitude: 36.0726354,
-      // zoom: 9,
-      zoom: 3,
+      zoom: 9,
     },
-    mapStyle: MAP_STYLE.mergeDeep({
-      sources: {
-        [KEY_PREFIX + 'l']: {type: 'geojson', data: LINE_GEOJSON},
-        [KEY_PREFIX + 'm']: {type: 'geojson', data: METER_GEOJSON},
-      },
-      layers: [{
-        id: KEY_PREFIX + 'l',
-        type: 'line',
-        source: KEY_PREFIX + 'l',
-        paint: {
-          'line-width': ['get', 'width'],
-          'line-color': 'yellow',
-          // 'line-cap': 'round',
-          // 'line-join': 'round',
-          'line-opacity': 0.5,
-        },
-      }, {
-        id: KEY_PREFIX + 'm',
-        type: 'circle',
-        source: KEY_PREFIX + 'm',
-        paint: {
-          'circle-radius': ['get', 'radius'],
-          'circle-color': 'blue',
-          'circle-stroke-color': 'white',
-          'circle-stroke-width': 1,
-          'circle-opacity': 0.5,
-        },
-      }],
-    }),
   }
 
   onViewportChange = viewport => {
@@ -55,35 +23,37 @@ class AssetMap extends Component {
   }
 
   render () {
-    /*
     const {
       selectedAssetTypeIds,
     } = this.props
-    */
     const {
-      mapStyle,
       viewport,
     } = this.state
-    /*
-    const layers = MAP_STYLE.get('layers').concat(
-      selectedAssetTypeIds.map(typeId => {
+    const mapStyle = MAP_STYLE.mergeDeep({
+      sources: {
+        [KEY_PREFIX + 'l']: {type: 'geojson', data: LINE_GEOJSON},
+        [KEY_PREFIX + 'm']: {type: 'geojson', data: METER_GEOJSON},
+      },
+      layers: selectedAssetTypeIds.map(typeId => {
         const isLine = typeId === 'l'
         return {
           id: KEY_PREFIX + typeId,
           type: isLine  ? 'line' : 'circle',
           source: KEY_PREFIX + typeId,
           paint: isLine ? {
-            'line-color': 'black',
             'line-width': ['get', 'width'],
+            'line-color': 'yellow',
+            'line-opacity': 0.8,
           } : {
-            'circle-color': 'yellow',
             'circle-radius': ['get', 'radius'],
+            'circle-color': 'blue',
+            'circle-stroke-color': 'white',
+            'circle-stroke-width': 1,
+            'circle-opacity': 0.8,
           },
         }
-      }))
-    this.setState({mapStyle: mapStyle.set('layers', layers)})
-    */
-
+      }),
+    })
     return (
       <ReactMapGL
         width='100%'
