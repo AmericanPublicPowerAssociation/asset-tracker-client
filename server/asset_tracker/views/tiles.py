1ea40723ada02f6c5ab054adf8cf5f1832fdc4ab
Hugo Aguirre <bul.ikana@gmail.com>
2019-02-19 17:28:00 -0600

    Get bounds for given indices

---
+++ server/asset_tracker/views/tiles.py
@@ -0,0 +1,12 @@
+from pyramid.view import view_config
+from pyramid.response import Response
+
+from tile_server import helper
+
+@view_config(route_name='tiles')
+def tiles(request):
+	x = int(request.matchdict['x'])
+	y = int(request.matchdict['y'])
+	z = int(request.matchdict['z'])
+	bounds = helper.indices_to_bounds(x, y, z)
+	return Response( 'West: ' + str(bounds.west) + ', South: ' + str(bounds.south) + ', East: ' + str(bounds.east) + ', North: ' + str(bounds.north) )
