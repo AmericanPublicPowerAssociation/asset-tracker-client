0ba65392f4e86920013474583b534b1fbfd4962d
salah ahmed <salah93@crosscompute.com>
2019-03-10 13:21:59 -0400

    save work

--- server/asset_tracker/__init__.py
+++ server/asset_tracker/__init__.py
@@ -5,7 +5,8 @@ from pyramid.request import Response
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
+    """
+        This function returns a Pyramid WSGI application.
     """
     with Configurator(settings=settings) as config:
         config.include('pyramid_jinja2')
@@ -13,7 +14,7 @@ def main(global_config, **settings):
         config.include('.routes')
         config.scan()
     # !!! QUICKFIX
-    # config.set_request_factory(request_factory)
+    config.set_request_factory(request_factory)
     return config.make_wsgi_app()
 
 
--- server/asset_tracker/views/default.py
+++ server/asset_tracker/views/default.py
@@ -1,11 +1,11 @@
 from pyramid.view import view_config
 # from pyramid.response import Response
 # from sqlalchemy.exc import DBAPIError
-# from sqlalchemy.orm import sessionmaker
-# from asset_tracker.models.asset import (engine, Asset, Base, VulnerableAsset)
+from sqlalchemy.orm import sessionmaker
+from asset_tracker.models.asset import (engine, Base, VulnerableAsset)
 
-# Base.metadata.bind = engine
-# DatabaseSession = sessionmaker(bind=engine)
+Base.metadata.bind = engine
+DatabaseSession = sessionmaker(bind=engine)
 
 
 @view_config(route_name='index', renderer='../templates/dashboard.jinja2')
@@ -13,17 +13,20 @@ def index(request):
     return {}
 
 
-"""
-@view_config(route_name='vulnerable_assets', renderer='json')
+@view_config(
+    request_method='GET',
+    route_name='vulnerable_assets',
+    renderer='json')
 def vulnerable_assets(request):
     db = request.dbsession
-    # hardcoded for now
-    user_id = 'alex_appa'
-    assets = db.query(Asset).innerjoin(
-        VulnerableAsset,
-        Asset.id == Vulnerable.asset_id).find(user_id=user_id)
-    return assets
-"""
+    product = request.GET.get('product', '')
+    vendor = request.GET.get('vendor', '')
+    version = request.GET.get('version', '')
+    # assets = db.query(Asset) \
+    # .join(VulnerableAsset, Asset.id == VulnerableAsset.asset_id).all()
+    assets = db.query(VulnerableAsset).all()
+    return assets + [{"name": product, "vendor": vendor, "version": version}]
+
 
 """
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
