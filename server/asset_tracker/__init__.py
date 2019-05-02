f7e59572e69439d75121d18f6cbc430d840c44ee
Roy Hyunjin Han <rhh@crosscompute.com>
2019-05-02 19:26:55 -0400

    Remove old files

--- server/asset_tracker/__init__.py
+++ server/asset_tracker/__init__.py
@@ -1,22 +1,3 @@
-from pyramid.config import Configurator
-
-from pyramid.request import Response, Request
-
-
-def main(global_config, **settings):
-    """
-        This function returns a Pyramid WSGI application.
-    """
-    with Configurator(settings=settings) as config:
-        config.include('pyramid_jinja2')
-        config.include('.models')
-        config.include('.routes')
-        config.scan()
-    # !!! QUICKFIX
-    config.set_request_factory(request_factory)
-    return config.make_wsgi_app()
-
-
 def request_factory(environ):
     # !!! QUICKFIX DO NOT USE IN PRODUCTION
     # Use only if in development mode not production
