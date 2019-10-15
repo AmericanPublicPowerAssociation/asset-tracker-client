bb13c61030e6e28614c07a7c46b3fa70f61c33b9
rmguarachi <rmguarachi@gmail.com>
2019-10-15 10:45:58 -0400

    added grids to riskswindow

--- src/components/RisksWindow.js
+++ src/components/RisksWindow.js
@@ -1,10 +1,29 @@
 import React, { useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import AssetsFilter from '../containers/AssetsFilter'
 import {
   RisksTable,
 } from 'asset-report-risks'
 
 
+const useStyles = makeStyles(theme => ({
+  grid: {
+    height: '100%',
+  },
+  frame: {
+    height: '100%',
+    [theme.breakpoints.down('xs')]: {
+      height: '50%',
+    },
+    overflow: 'auto',
+  },
+}))
+
+
 export default function RisksWindow(props) {
+  const classes = useStyles()
+
   const {
     refreshRisks,
     risks,
@@ -17,10 +36,18 @@ export default function RisksWindow(props) {
   }, [refreshRisks])
 
   return (
-    <RisksTable
-      risks={risks}
-      openTaskEditDialog={openTaskEditDialog}
-      setEditingTaskValues={setEditingTaskValues}
-    />
+    <Grid container className={classes.grid}>
+      <Grid item className={classes.frame}
+        xs={12} sm={5} md={4} lg={3} xl={2} >
+        <AssetsFilter />
+      </Grid>
+      <Grid item className={classes.frame}
+        xs={12} sm={7} md={8} lg={9} xl={10} >
+        <RisksTable
+          risks={risks}
+          openTaskEditDialog={openTaskEditDialog}
+          setEditingTaskValues={setEditingTaskValues} />
+      </Grid>
+    </Grid>
   )
 }
