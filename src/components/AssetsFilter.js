a8064d1fdc92d531ccb5b8f51ed585ff7aa7b923
rmguarachi <rmguarachi@gmail.com>
2019-10-30 12:03:16 -0400

    removed button import

--- src/components/AssetsFilter.js
+++ src/components/AssetsFilter.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
