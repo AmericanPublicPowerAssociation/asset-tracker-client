240a22654660e395f9b0dddeb79b87b85ed40b4b
Rodrigo Guarachi <rmguarachi@crosscompute.com>
2019-11-26 17:27:57 +0000

    Added react-router and redux-saga

--- experiments/asset-tracker-client/src/index.js
+++ experiments/asset-tracker-client/src/index.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { applyMiddleware, compose, createStore } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import { config as configureEnvironment } from 'dotenv'
 import reduceState from './reducers'
+import reduceSaga from './sagas'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 const initialState = {}
-// const enhanceStore = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reduceState, initialState)
+const enhanceStore = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(reduceState, initialState, enhanceStore(
+  applyMiddleware(
+    sagaMiddleware,
+  )))
 
 configureEnvironment()
+sagaMiddleware.run(reduceSaga)
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root'))
 serviceWorker.unregister()
