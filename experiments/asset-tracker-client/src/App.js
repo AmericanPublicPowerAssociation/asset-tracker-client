d754255384d884cbbd94c4a69b4b01a98b4eadeb
Rodrigo Guarachi <rmguarachi@crosscompute.com>
2019-11-26 17:15:28 +0000

    added deckgl editable layer

--- experiments/asset-tracker-client/src/App.js
+++ experiments/asset-tracker-client/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
-// import React, { useState } from 'react'
-// import DeckGL from '@deck.gl/react'
-// import { EditableGeoJsonLayer } from '@nebula.gl/layers'
-// import { StaticMap } from 'react-map-gl'
+import React, { useState } from 'react'
+import DeckGL from '@deck.gl/react'
+import { EditableGeoJsonLayer } from '@nebula.gl/layers'
+import { StaticMap } from 'react-map-gl'
 
-function App() {
+const initialItems = {
+  type: 'FeatureCollection',
+  features: []
+}
+
+const viewState = {
+  longitude: -73.897052,
+  latitude: 40.780474,
+  zoom: 15
+}
+
+function App(props) {
+  const [items, setItems] = useState(initialItems) 
+  const [selectedItems, setSelectedItems] = useState([])
+  const [initialViewState, setInitialViewState] = useState(viewState)
+  const [drawMode, setDrawMode] = useState('')
+
   /*
   const [geojson, setGeojson] = useState({
     type: 'FeatureCollection',
@@ -16,9 +31,21 @@ function App() {
     </DeckGL>
   )
   */
+  const layers = []
+  layers.push(new EditableGeoJsonLayer({
+    id: 'draw-layer',
+    data: items,
+    selectedFeatureIndexes: selectedItems
+  }))
   return (
     <div>
-      whee
+      <DeckGL
+        initialViewState={initialViewState}
+        layers={layers}>
+      <StaticMap
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        />
+      </DeckGL>
     </div>
   )
 }
