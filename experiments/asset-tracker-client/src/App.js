cbda1a2f179ac1bc511c97ca2f2639c1bf346966
Rodrigo Guarachi <rmguarachi@crosscompute.com>
2019-11-26 16:45:12 +0000

    Add basic redux structure

--- experiments/asset-tracker-client/src/App.js
+++ experiments/asset-tracker-client/src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
-import DeckGL from '@deck.gl/react'
-import { EditableGeoJsonLayer } from '@nebula.gl/layers'
-import { StaticMap } from 'react-map-gl'
+import React from 'react'
+// import React, { useState } from 'react'
+// import DeckGL from '@deck.gl/react'
+// import { EditableGeoJsonLayer } from '@nebula.gl/layers'
+// import { StaticMap } from 'react-map-gl'
 
 function App() {
+  /*
   const [geojson, setGeojson] = useState({
     type: 'FeatureCollection',
     features: [],
@@ -12,6 +14,12 @@ function App() {
     <DeckGL>
       <StaticMap mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} />
     </DeckGL>
+  )
+  */
+  return (
+    <div>
+      whee
+    </div>
   )
 }
 
--- experiments/asset-tracker-client/src/index.js
+++ experiments/asset-tracker-client/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import { config as configureEnvironment } from 'dotenv'
+import reduceState from './reducers'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+const initialState = {}
+// const enhanceStore = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reduceState, initialState)
+
 configureEnvironment()
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'))
 serviceWorker.unregister()
