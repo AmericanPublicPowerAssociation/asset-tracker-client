8cdaa37ea10065962c7fa1fb6eca43b5d8b87f24
Roy Hyunjin Han <rhh@crosscompute.com>
2019-11-26 11:00:53 -0500

    Add dotenv

--- experiments/asset-tracker-client/src/App.js
+++ experiments/asset-tracker-client/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DeckGL from '@deck.gl/react'
+import { EditableGeoJsonLayer } from '@nebula.gl/layers'
+import { StaticMap } from 'react-map-gl'
 
 function App() {
+  const [geojson, setGeojson] = useState({
+    type: 'FeatureCollection',
+    features: [],
+  })
   return (
-    'Whee'
+    <DeckGL>
+      <StaticMap mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} />
+    </DeckGL>
   )
 }
 
--- experiments/asset-tracker-client/src/index.js
+++ experiments/asset-tracker-client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { config as configureEnvironment } from 'dotenv'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+configureEnvironment()
 ReactDOM.render(<App />, document.getElementById('root'))
 serviceWorker.unregister()
