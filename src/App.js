77c2c5031a8b60c247d5257d7de4f83bf2672ecd
Roy Hyunjin Han <rhh@crosscompute.com>
2019-11-27 09:02:03 -0500

    Add example data

--- src/App.js
+++ src/App.js
@@ -25,6 +25,41 @@ import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 
 const TOOLTIP_DELAY = 500
+const ASSETS = [{
+  id: 'akM1',
+  name: 'High Voltage Transformer 1',
+  type: 'tp',
+  vendor: 'Schneider Electric',
+  product: 'HVT36A',
+  version: '2.0.1',
+}, {
+  id: 'anZQ',
+  name: 'Meter 1',
+  type: 'm',
+  vendor: 'ITRON',
+  product: '6219399',
+  version: '7.9.5',
+}]
+const TASKS = [{
+  id: 1,
+  name: 'Clean Transformer',
+  status: 50,
+}, {
+  id: 2,
+  name: 'Reset Meter',
+  status: 0,
+}]
+const RISKS = [{
+  id: 1,
+  name: 'Open Port',
+  meterCount: 5,
+  assetId: 'anZQ',
+}, {
+  id: 2,
+  name: 'Voltage Too High',
+  meterCount: 2,
+  assetId: 'akM1',
+}]
 
 const useStyles = makeStyles(theme => ({
   usersWindow: {
@@ -39,7 +74,7 @@ const useStyles = makeStyles(theme => ({
     left: 0,
     padding: theme.spacing(1),
   },
-  layersWindow: {
+  overlaysWindow: {
     position: 'fixed',
     top: theme.spacing(8),
     left: theme.spacing(1),
@@ -49,16 +84,16 @@ const useStyles = makeStyles(theme => ({
   },
   filtersWindow: {
     position: 'fixed',
-    top: theme.spacing(30),
+    top: theme.spacing(29),
     left: theme.spacing(1),
-    bottom: theme.spacing(5),
+    bottom: theme.spacing(38),
     width: theme.spacing(30),
     padding: theme.spacing(1),
     zIndex: 1,
   },
   rowsWindow: {
     position: 'fixed',
-    left: theme.spacing(34),
+    left: theme.spacing(1),
     bottom: theme.spacing(5),
     right: theme.spacing(34),
     height: theme.spacing(30),
@@ -99,6 +134,7 @@ const initialViewState = {
 function App() {
   const classes = useStyles()
 
+  const [selectedOverlay, setSelectedOverlay] = useState('Assets')
   const [withFilters, setWithFilters] = useState(true)
   const [withRows, setWithRows] = useState(true)
   const [withDetails, setWithDetails] = useState(true)
@@ -111,6 +147,95 @@ function App() {
     id: 'editable-geojson-layer',
     data: initialGeojson,
   }))
+
+  const visibleAssetCount = ASSETS.length
+  const visibleTaskCount = TASKS.length
+  const visibleRiskCount = RISKS.length
+
+  const assetsOverlayLabel = `Assets (${visibleAssetCount})`
+  const tasksOverlayLabel = `Tasks (${visibleTaskCount})`
+  const risksOverlayLabel = `Risks (${visibleRiskCount})`
+
+  let table
+  switch(selectedOverlay) {
+    case 'Assets': {
+      table = (
+        <Table size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Asset</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Vendor</TableCell>
+              <TableCell>Product</TableCell>
+              <TableCell>Version</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {ASSETS.map(asset => 
+            <TableRow>
+              <TableCell>{asset.name}</TableCell>
+              <TableCell>{asset.type}</TableCell>
+              <TableCell>{asset.vendor}</TableCell>
+              <TableCell>{asset.product}</TableCell>
+              <TableCell>{asset.version}</TableCell>
+            </TableRow>
+          )}
+          </TableBody>
+        </Table>
+      )
+      break
+    }
+    case 'Tasks': {
+      table = (
+        <Table size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Task</TableCell>
+              <TableCell>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {TASKS.map(task =>
+            <TableRow>
+              <TableCell>{task.name}</TableCell>
+              <TableCell>{{
+                0: 'New',
+                50: 'In Progress',
+              }[task.status]}</TableCell>
+            </TableRow>
+          )}
+          </TableBody>
+        </Table>
+      )
+      break
+    }
+    case 'Risks': {
+      table = (
+        <Table size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Risk</TableCell>
+              <TableCell>Asset</TableCell>
+              <TableCell>Customers</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {RISKS.map(risk =>
+            <TableRow>
+              <TableCell>{risk.name}</TableCell>
+              <TableCell>{risk.assetId}</TableCell>
+              <TableCell>{risk.meterCount}</TableCell>
+            </TableRow>
+          )}
+          </TableBody>
+        </Table>
+      )
+      break
+    }
+    default:
+      break
+  }
+
   return (
     <div>
 
@@ -160,45 +285,28 @@ function App() {
 
       </div>
 
-      <div className={classes.layersWindow}>
-        <RadioGroup>
-          <FormControlLabel value="assets" control={<Radio />} label="Assets" />
-          <FormControlLabel value="tasks" control={<Radio />} label="Tasks" />
-          <FormControlLabel value="risks" control={<Radio />} label="Risks" />
+      <div className={classes.overlaysWindow}>
+        {/* TODO: Show counts for what is visible in map after applying filters */}
+        <RadioGroup value={selectedOverlay} onChange={e => setSelectedOverlay(e.target.value)}>
+          <FormControlLabel value='Assets' control={<Radio />} label={assetsOverlayLabel} />
+          <FormControlLabel value='Tasks' control={<Radio />} label={tasksOverlayLabel} />
+          <FormControlLabel value='Risks' control={<Radio />} label={risksOverlayLabel} />
         </RadioGroup>
       </div>
 
     {withFilters &&
       <Paper className={classes.filtersWindow}>
         <CloseIcon className={classes.closeButton} onClick={() => setWithFilters(false)} />
-        Filters
+        Filters for {selectedOverlay}
       </Paper>
     }
 
     {withRows &&
       <Paper className={classes.rowsWindow}>
-
         <CloseIcon className={classes.closeButton} onClick={() => setWithRows(false)} />
-
-        <div>
-        </div>
-
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>A</TableCell>
-              <TableCell>B</TableCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            <TableRow>
-              <TableCell>a</TableCell>
-              <TableCell>b</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-
+        {/* TODO: Show only what is visible in the map */}
+        {/* TODO: Implement paging */}
+        {table}
       </Paper>
     }
 
