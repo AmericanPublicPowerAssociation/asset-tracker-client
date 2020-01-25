dc2d3d14ab3cff548f4fced78ac83e2974c0d66c
Roy Hyunjin Han <rhh@crosscompute.com>
2020-01-24 22:52:12 -0500

    Put buses on new assets

--- src/macros/randomMacros.js
+++ src/macros/randomMacros.js
@@ -5,6 +5,12 @@ const ALPHABET = [
 ]
 const ALPHABET_LENGTH = ALPHABET.length
 
+export function getRandomId(minimumLength) {
+  const monotonicallyIncreasingNumber = Date.now()
+  const randomString = getRandomString(minimumLength)
+  return randomString + monotonicallyIncreasingNumber
+}
+
 export function getRandomString(length) {
   // https://gist.github.com/6174/6062387#gistcomment-2993079
   return [...Array(length)]
