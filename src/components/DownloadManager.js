8694a78a60f6a6be3a57c2b4994decce07a8c38b
Miguel G <miguel.gordian@devlabs.com.mx>
2020-02-18 08:03:36 +0000

    Select by default the first asset on the list

--- src/components/DownloadManager.js
+++ src/components/DownloadManager.js
@@ -1,25 +1,17 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { withStyles } from '@material-ui/core/styles'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogActions from '@material-ui/core/DialogActions'
 import Button from '@material-ui/core/Button'
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Grid from "@material-ui/core/Grid";
-import {TextField} from "@material-ui/core";
 import FormControl from '@material-ui/core/FormControl'
 import NativeSelect from '@material-ui/core/NativeSelect'
 import Input from '@material-ui/core/Input'
-import InputLabel from '@material-ui/core/InputLabel'
 import Typography from "@material-ui/core/Typography"
 
 import {
   getAssetTableData,
-  getAssetsGeoJson,
 } from '../selectors'
 
 
@@ -31,9 +23,14 @@ const DownloadManager = (props) => {
 	onOk
     } = props;
 
-  const [powerId, setPowerId] = useState(null)
-  const { head, data, name } = useSelector(getAssetTableData)
+    const { data } = useSelector(getAssetTableData)
+    const [powerId, setPowerId] = useState('')
+
+    if (data !== null && data !== undefined && data.length > 0 && powerId === '') {
+	setPowerId(data[0].id)
+    }
     
+	
     return (<Dialog
         open={open}
         onClose={onClose}
