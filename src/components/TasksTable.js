caa1c8ad3ee002b93e1360c2fe20d588e4fb2c2f
rmguarachi <rmguarachi@gmail.com>
2020-03-17 15:07:00 -0400

    taskstable with material table

--- src/components/TasksTable.js
+++ src/components/TasksTable.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { useSelector } from 'react-redux'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
+import MaterialTable from 'material-table'
+import AddBox from '@material-ui/icons/AddBox'
+import ArrowDownward from '@material-ui/icons/ArrowDownward'
+import Check from '@material-ui/icons/Check'
+import ChevronLeft from '@material-ui/icons/ChevronLeft'
+import ChevronRight from '@material-ui/icons/ChevronRight'
+import Clear from '@material-ui/icons/Clear'
+import DeleteOutline from '@material-ui/icons/DeleteOutline'
+import Edit from '@material-ui/icons/Edit'
+import FilterList from '@material-ui/icons/FilterList'
+import FirstPage from '@material-ui/icons/FirstPage'
+import LastPage from '@material-ui/icons/LastPage'
+import Remove from '@material-ui/icons/Remove'
+import SaveAlt from '@material-ui/icons/SaveAlt'
+import Search from '@material-ui/icons/Search'
+import ViewColumn from '@material-ui/icons/ViewColumn'
 import {
   getTaskPriorityLabel,
   getTaskStatusLabel,
@@ -12,6 +23,27 @@ import {
 import {
   getOpenTaskById,
 } from '../selectors'
+
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+}
 
 const TASK_TABLE_COLUMN_NAMES = [
   'name',
@@ -22,6 +54,7 @@ const TASK_TABLE_COLUMN_NAMES = [
 
 export default function TasksTable(props) {
   const taskById = useSelector(getOpenTaskById)
+  const { getHeaderLabel } = props
 
   const data = Object.values(taskById).map(
     task => {
@@ -32,46 +65,18 @@ export default function TasksTable(props) {
       }
   })
 
-  const head = TASK_TABLE_COLUMN_NAMES
+  const columns = TASK_TABLE_COLUMN_NAMES.map( field => {
+    return {field, title: getHeaderLabel(field)}
+  })
 
-  const name = 'task'
-
-  function getHeaderLabel(header) {
-    const result = header.replace( /([A-Z])/g, " $1" );
-    var headerLabel = result.charAt(0).toUpperCase() + result.slice(1);
-    return headerLabel
-  }
-
+  const tableName = 'Tasks'
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          { head.map( header => {
-            const key = `table-${name}-${header}`
-            return <TableCell key={key} align='center'>{getHeaderLabel(header)}</TableCell>
-          })}
-        </TableRow>
-      </TableHead>
-
-      <TableBody>
-      {data.map(task => {
-        return (
-          <TableRow key={task.id}>
-            { head.map(header => {
-              const key = `table-${name}-${header}-${task.id}`
-              return (
-                <TableCell
-                  align='center'
-                  key={key}
-                >
-                  {task[header]}
-                </TableCell>
-              )
-            })}
-          </TableRow>
-        )}
-      )}
-      </TableBody>
-    </Table>
+    <MaterialTable
+      icons={tableIcons}
+      title={tableName}
+      options={ {search: true} }
+      columns={columns}
+      data={data}
+    />
   )
 }
