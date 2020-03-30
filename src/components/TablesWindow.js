7008a804186de2c2692ca52d78bbf4450e732f74
rmguarachi <rmguarachi@gmail.com>
2020-03-30 17:11:58 -0400

    remove padding tasksTable

--- src/components/TablesWindow.js
+++ src/components/TablesWindow.js
@@ -21,7 +21,6 @@ const useStyles = makeStyles(theme => ({
     right: theme.spacing(1),
     height: '33%',
     bottom: theme.spacing(5),
-    //padding: theme.spacing(1),
     overflow: 'auto',
   },
 }))
