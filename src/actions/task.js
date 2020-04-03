84aabaa72527a72a43ab69e6a46972fa7bd1df14
rmguarachi <rmguarachi@gmail.com>
2020-04-02 22:27:47 -0400

    setTaskName will save name in the db

--- src/actions/task.js
+++ src/actions/task.js
@@ -1,7 +1,9 @@
 import {
-  SET_TASK_NAME,
+  UPDATE_TASK,
 } from '../constants'
 
-export function setTaskName(id, name) {
-  return {type: SET_TASK_NAME, payload: {id, name}}
+export function setTaskName(task_id, name, priority, status) {
+  return {
+    type: UPDATE_TASK,
+    payload: {task_id, name, priority, status}}
 }
