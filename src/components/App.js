3b8ca762ab84f8a23741d309ec51b9d4925bacf9
Rodrigo <rmGuarachi@gmail.com>
2020-05-13 20:21:25 -0400

    removed extra OverlaysWindow component

--- src/components/App.js
+++ src/components/App.js
@@ -104,7 +104,6 @@ export default function App() {
         setIsWithTables={setIsWithTables}
       />
       <OverlaysWindow />
-      <OverlaysWindow />
     {isWithDetails &&
       <DetailsWindow
         isWithDetails={isWithDetails}
