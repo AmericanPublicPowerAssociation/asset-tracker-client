6c1f9ae6c07597c073c015215828cc3cf69c2697
Polina <polina@crosscompute.com>
2020-08-09 03:33:28 +0000

    Changed SET_ASSET to return SKETCH_MODE_EDIT instead of ADD

--- src/reducers/sketchMode.js
+++ src/reducers/sketchMode.js
@@ -1,7 +1,5 @@
 import {
-  // SKETCH_MODE_ADD,
-  // SET_ASSET,
-  SET_ASSETS_GEOJSON,
+  SET_ASSET,
   SET_SKETCH_MODE,
   SKETCH_MODE,
   SKETCH_MODE_EDIT,
@@ -14,10 +12,7 @@ export default function sketchMode(state=initialState, action) {
     case SET_SKETCH_MODE: {
       return action.payload
     }
-    /*case SET_ASSET: {
-      return SKETCH_MODE_ADD
-    }*/
-    case SET_ASSETS_GEOJSON: {
+    case SET_ASSET: {
       return SKETCH_MODE_EDIT
     }
     default: {
